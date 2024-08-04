4905333828be15ce8b3521bdba248631d772ea4d
AlbaHoyo <alba.maria.hoyo@gmail.com>
2024-08-04 17:46:07 +0200

    v3

--- controllers/petController.js
+++ controllers/petController.js
@@ -1,10 +1,12 @@
 const Pet = require('../models/Pet');
 
+// Crear una nueva mascota
 exports.createPet = async (req, res) => {
   const { name, species, breed, age, size, personality, preferences, photo_url } = req.body;
+
   try {
+    // Crear una nueva mascota
     const newPet = new Pet({
-      user_id: req.user,
       name,
       species,
       breed,
@@ -14,6 +16,7 @@ exports.createPet = async (req, res) => {
       preferences,
       photo_url,
     });
+
     await newPet.save();
     res.status(201).json(newPet);
   } catch (error) {
@@ -21,20 +24,22 @@ exports.createPet = async (req, res) => {
   }
 };
 
+// Obtener mascotas del usuario autenticado
 exports.getPets = async (req, res) => {
   try {
-    const pets = await Pet.find({ user_id: req.user });
+    const pets = await Pet.find();
     res.json(pets);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+// Obtener todas las mascotas con información del propietario
 exports.getAllPets = async (req, res) => {
-    try {
-      const pets = await Pet.find().populate('owner', 'email location'); 
-      res.json(pets);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+  try {
+    const pets = await Pet.find();
+    res.json(pets);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
